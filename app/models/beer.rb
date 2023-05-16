4f896743b2a8c619582c553c71de3a95ae56ab63
Eli Da Costa <costa141@hotmail.co.uk>
2023-05-16 22:05:58 +0100

    added associations and validations beer

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,9 @@
 class Beer < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
+  has_many :reviews
+  has_many :orders
+
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :description, presence: true
 end
