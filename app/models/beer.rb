db691aeb0454d7bfcb86c311c7e3d77a1e291c00
Alex Williams <ahnnwilliams@hotmail.co.uk>
2023-05-18 21:24:03 +0100

    Added orders controller, index and destroy actions, and index view

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,6 +1,7 @@
 class Beer < ApplicationRecord
   belongs_to :user
-  has_many :orders
+  # changed from has_many :orders
+  belongs_to :order
 
   validates :name, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
