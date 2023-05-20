1c320198b6bd7440c42ffe38fa5307c0432023d3
maram allaghi <writtenbymaram@gmail.com>
2023-05-20 15:22:42 +0100

    created order view and changed index controller action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,12 @@
 class OrdersController < ApplicationController
   # before_action :set_order, only: %i[destroy]
   def index
+    @user = current_user
     @orders = Order.all
+    @order = @orders.where(user_id: @user)
+    @beer = Beer.find(@order[0].beer_id)
   end
-
+  
   def destroy
     @order = Order.find(params[:id])
     @order.destroy!
