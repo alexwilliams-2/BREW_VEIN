8cf9a15c9a2186e87d57210b19e7cd963fec52cd
Alex Williams <ahnnwilliams@hotmail.co.uk>
2023-05-20 16:52:44 +0100

    failed attempt

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,35 @@
 class OrdersController < ApplicationController
-  # before_action :set_order, only: %i[destroy]
+  before_action :set_beer, only: %i[create]
+
   def index
     @user = current_user
-    @orders = Order.all
-    @order = @orders.where(user_id: @user)
-    @beer = Beer.find(@order[0].beer_id)
+    @orders = Order.where(user_id: @user)
+    # @order = @orders.where(user_id: @user)
+    # @beer = Beer.find(@order[0].beer_id)
+
   end
-  
+
   def destroy
     @order = Order.find(params[:id])
     @order.destroy!
     # redirect so tmewhere
   end
 
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new
+    @order.beer = @beer
+    @order.user = current_user
+  end
+
+  private
+
+  def set_beer
+    @beer = Beer.find(params[:id])
+  end
   # def set_order
   #   @order = Order.find(params[:id])
   # end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :beers, only: [:index, :show, :new, :create, :destroy]
-  resources :orders, only: %i[index destroy]
+  resources :beers, only: [:index, :show, :new, :create, :destroy] do
+    resources :orders, only: %i[index destroy new create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
