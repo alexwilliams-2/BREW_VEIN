4a627257070e2d98cc28c680006637ae04288388
maram allaghi <writtenbymaram@gmail.com>
2023-05-20 11:30:07 +0100

    fixed duplicate controller issue

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,9 +1,41 @@
 class BeersController < ApplicationController
+  before_action :set_user, only: %i[new create]
+
   def index
     @beers = Beer.all
   end
 
   def show
     @beer = Beer.find(params[:id])
+  end
+
+  def new
+    @beer = Beer.new
+  end
+
+  def create
+    @beer = Beer.new(beer_params)
+    @beer.user = @user
+    @beer.save
+    if @beer.save
+      redirect_to beer_path(@beer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    beer = Beer.find(params[:id])
+    beer.destroy
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def beer_params
+    params.require(:beer).permit(:name, :price, :description)
   end
 end
