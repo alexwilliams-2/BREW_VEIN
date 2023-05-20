cbe435d79ab5a361f683bcfb928fc61b1c2fb57a
maram allaghi <writtenbymaram@gmail.com>
2023-05-20 12:16:44 +0100

    created user and beer db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
+
+10.times do
+  user = User.new(name: Faker::Name.name , email: Faker::Internet.email, password: Faker::Internet.password ,description: Faker::Beer.brand)
+  user.save
+  "#{user.id} has been created"
+end
+
+
 5.times do
-  Beer.new(title: Faker::Music.band, url: Faker::Sports::Football.player, votes: 2)
+  beer = Beer.new(name: Faker::Beer.name, price: rand(2...10), description: Faker::Beer.style, user_id:rand(1...10))
+  beer.save
+  "#{beer.id} has been created"
 end
