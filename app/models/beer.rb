5186b768513383b1651efe33e11ac4416218cfc2
maram allaghi <writtenbymaram@gmail.com>
2023-05-20 11:55:38 +0100

    fixed models to be able to create order and beer

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,7 +1,7 @@
 class Beer < ApplicationRecord
   belongs_to :user
   # changed from has_many :orders
-  belongs_to :order
+  # belongs_to :order
 
   validates :name, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
